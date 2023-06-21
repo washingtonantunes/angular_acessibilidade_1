f9f74d951ea5eb8b294a945875a2b8cf1dfdfb03
Washington Antunes <washington.cead@gmail.com>
2023-06-21 19:18:13 -0300

    criando um formulário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { YesNoButtonGroupModule } from './shared/components/yes-no-button-group/yes-no-button-group.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, YesNoButtonGroupModule],
+  imports: [BrowserModule, YesNoButtonGroupModule, ReactiveFormsModule],
 })
 export class AppModule {}
